63b977d9b220b9d3a6b42c28a6a05fb1144de306
gokulkrishnakb <gokulkrishnakb070@gmail.com>
2025-10-20 18:04:30 +0530

    second backend commit

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://localhost:5174",
         "http://127.0.0.1:5174",
-        "https://kitchentreasures.netlify.app",
+        "https://kitchentreasures1.netlify.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
