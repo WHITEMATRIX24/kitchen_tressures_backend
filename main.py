c17ead7dd4a77779a03e1d0ef725f133b8446964
gokulkrishnakb <gokulkrishnakb070@gmail.com>
2025-10-20 21:48:10 +0530

    updated backend URLs to 13.60.105.81

--- main.py
+++ main.py
@@ -19,15 +19,7 @@ app = FastAPI(title="Route Planner API")
 # =====================================================
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "http://localhost:5174",
-        "http://127.0.0.1:5174",
-        "https://kitchentreasures1.netlify.app",
-        "https://kitchentreasures2.netlify.app",
-      
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
